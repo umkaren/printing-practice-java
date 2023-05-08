d06242ae04940ffd71fa38929d09b60ae94fa1f1
Karen Ngo <uminhngo@gmail.com>
2023-05-08 18:26:10 -0400

    Initial commit

---
+++ src/com/example/mynameis/MyNameIs.java
@@ -0,0 +1,25 @@
+//first class
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is");
+        System.out.println("Karen");
+        System.out.println("Ngo");
+    }
+}
+
+//second class
+class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I'd like to visit");
+        System.out.println("Osaka");
+        System.out.println("Japan");
+    }
+}
+
+//third class
+class HiFriend {
+    public static void main(String[] args) {
+        String friendName = "Elmo";
+        System.out.print("Hi, " + friendName + "!");
+    }
+}
